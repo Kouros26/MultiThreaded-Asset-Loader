db2e1fa52dbea57a6051fa5829dab480f237ba34
Angel-2180 <a.gimer@student.isartdigital.com>
2022-12-15 01:51:51 -0500

    Code Norme

--- Core/include/ThreadPool.h
+++ Core/include/ThreadPool.h
@@ -8,17 +8,16 @@ class ThreadPool
 {
 public:
 
-    ThreadPool();
-    ~ThreadPool();
-    void AddToQueue(const std::function<void()>& task);
+	ThreadPool();
+	~ThreadPool();
+	void AddToQueue(const std::function<void()>& task);
 
 private:
 
-    void ThreadLoop();
-
-    bool shouldStop = false;
-    std::mutex queueMutex;
-    std::condition_variable mutexCondition;
-    std::vector<std::thread> threads;
-    std::queue<std::function<void()>> tasksQueue;
+	void ThreadLoop();
+	bool shouldStop = false;
+	std::mutex queueMutex;
+	std::condition_variable mutexCondition;
+	std::vector<std::thread> threads;
+	std::queue<std::function<void()>> tasksQueue;
 };
